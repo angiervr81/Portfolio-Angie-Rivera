ee034bed44685b2c7b8a087bd8f16044d37f20ef
Angie <Angie.Rivera81@bcmail.cuny.edu>
2025-03-03 13:52:07 -0500

    Start of JS file

---
+++ Public/JS/main.js
@@ -0,0 +1,8 @@
+
+// Get the canvas element and its context
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+//Loading the background image
+const background = new Image();
+background.src = 'Images/';
