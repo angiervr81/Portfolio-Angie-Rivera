35e1636908d04c95b052f0842d1895c1d09dfef1
Angie <Angie.Rivera81@bcmail.cuny.edu>
2025-03-04 14:43:28 -0500

    Adding background images

--- Portfolio/JS/sceneproject.js
+++ Portfolio/JS/sceneproject.js
@@ -1,8 +1,37 @@
 
 // Get the canvas element and its context
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
 //Loading the background image
 const background = new Image();
-background.src = 'Images/';
+background.src = '../images/sea-water-ocean-wave-inside-view-generative-ai-free-photo.jpg';
+
+/*
+//Loading foreground images
+const foreground = new Image();
+const foreground2 = new Image();
+foreground.src = '../images/foreground.png';
+foreground2.src = '../images/foreground.png';
+**/
+
+
+// Draw everything once images are loaded
+background.onload =() =>{
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+};
+/*
+foreground.onload =() =>{
+    ctx.drawImage(foreground, 0, 0, canvas.width, canvas.height);
+};
+foreground2.onload =() =>{
+    ctx.drawImage(foreground2, 0, 0, canvas.width, canvas.height);
+};
+**/
+
+window.onload = function() {
+ctx.fillStyle = 'blue';
+ctx.font = '60px cursive', 'bold'; // Set font size and style
+ctx.fillText('Angie Rivera', 100, 50); // Draw text
+ctx.fillText('Scence Assignment', 100, 100);
+}
