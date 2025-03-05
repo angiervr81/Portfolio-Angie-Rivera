ee1f52c869cc24ee1b89bc3d36824d1201ee98df
Angie <Angie.Rivera81@bcmail.cuny.edu>
2025-03-05 18:11:30 -0500

    Updating sceneproject.js
    changed image used krita to practice background and foreground

--- Portfolio/JS/sceneproject.js
+++ Portfolio/JS/sceneproject.js
@@ -5,7 +5,7 @@ const ctx = canvas.getContext('2d');
 
 //Loading the background image
 const background = new Image();
-background.src = 'images/landscape.jpg';
+background.src = 'images/BFSCENE.jpg';
 
 
 /*Loading foreground images
